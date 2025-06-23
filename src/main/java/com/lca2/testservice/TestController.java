d725799726fda89b25bbc5ab8613558d7c154f38
minju26 <mjalswn26@gmail.com>
2025-06-23 15:57:58 +0900

    feat: /api/test 엔드포인트 요청 매핑 추가

--- src/main/java/com/lca2/testservice/TestController.java
+++ src/main/java/com/lca2/testservice/TestController.java
@@ -1,14 +1,17 @@
 package com.lca2.testservice;
 
 import com.lca2.testservice.web.context.GatewayRequestHeaderUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/healthy")
     public ResponseEntity<String> testGatewayHeaders() {
         String userId = GatewayRequestHeaderUtils.getUserId();
 
