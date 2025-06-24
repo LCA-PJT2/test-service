6d01a96f28075809c7a783577221ceac320e2a84
minju26 <mjalswn26@gmail.com>
2025-06-24 10:29:37 +0900

    chore: 라우팅 테스트

--- src/main/java/com/lca2/testservice/TestController.java
+++ src/main/java/com/lca2/testservice/TestController.java
@@ -1,18 +1,22 @@
 package com.lca2.testservice;
 
 import com.lca2.testservice.web.context.GatewayRequestHeaderUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
 
     @GetMapping("/healthy")
     public ResponseEntity<String> testGatewayHeaders() {
+        log.info("test gateway routing");
+
         String userId = GatewayRequestHeaderUtils.getUserId();
 
         if (userId == null) {
